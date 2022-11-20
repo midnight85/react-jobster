cf947a1b247895a8a9838ae2d16e7eff28baa98a
midnight85 <maksvell2407@gmail.com>
2022-11-20 22:48:21 +0200

    Add job page, functionality for adding job

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import userSlice from "./features/user/userSlice";
 import sidebarSlice from "./features/sidebar/sidebarSlice";
+import jobSlice from "./features/job/jobSlice";
 
 export const store = configureStore({
   reducer: {
     user: userSlice,
     sidebar: sidebarSlice,
+    job: jobSlice,
   },
 });
