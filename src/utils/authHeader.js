face99b5633dee47aa570a29b4615351420d2c56
midnight85 <maksvell2407@gmail.com>
2022-12-14 23:54:40 +0200

    fix auth token

--- src/utils/authHeader.js
+++ src/utils/authHeader.js
@@ -1,7 +1,7 @@
 const authHeader = (thunkAPI) => {
   return {
     headers: {
-      authorization: `Bearer${thunkAPI.getState().user.user.token}`,
+      authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
     },
   };
 };
